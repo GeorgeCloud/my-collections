e2be96957072c11794f57e4137f73fe2fb216afa
GeorgeCloud <net.george.sky@gmail.com>
2023-02-08 23:03:08 -0800

    add: React scaffold / Navbar / react-router-dom pkg

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import '../styles/NavBar.css'
+
+function NavBar(){
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <p>MyCollections</p>
+                </li>
+
+                <li>
+                    <p>Home</p>
+                </li>
+                <li>
+                    <p>Collections</p>
+                </li>
+                <li>
+                    <p>Settings</p>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
